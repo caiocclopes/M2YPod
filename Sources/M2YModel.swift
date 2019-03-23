9517830767a19c0ffe843c9db4108bf6fd4d328f
Caio Lopes <caiotuxo@gmail.com>
2019-03-23 13:14:49 -0300

    fix

---
+++ Sources/M2YModel.swift
@@ -0,0 +1,25 @@
+//
+//  M2YModel.swift
+//  Mobile2you
+//
+//  Created by Mobile2You on 02/05/2018.
+//  Copyright © 2018 Mobile2You. All rights reserved.
+//
+
+import Foundation
+import EVReflection
+
+class M2YModel: EVNetworkingObject {
+    
+    override func skipPropertyValue(_ value: Any, key: String) -> Bool {
+        if let value = value as? String, value.count == 0 || value == "null" {
+            return true
+        } else if let value = value as? NSArray, value.count == 0 {
+            return true
+        } else if value is NSNull {
+            return true
+        }
+        return false
+    }
+    
+}
